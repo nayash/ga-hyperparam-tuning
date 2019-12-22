96d76ec80f6540e5d22035f560bfbe99a2b9829e
asunayak <lotus.nayak@gmail.com>
2019-12-22 16:48:19 +0530

    b4 updating gitignore

--- src/scratch_pad.py
+++ src/scratch_pad.py
@@ -3,15 +3,13 @@ import numpy as np
 # from keras.utils import np_utils
 # from sklearn.preprocessing import LabelEncoder
 
-from utils import *
+from utils import get_readable_ctime
 import os
 import pandas as pd
 import threading
-from multiprocessing import Process
+import multiprocessing as mp
 import time
 
-print(os.getcwd())
-print(get_readable_ctime())
 def rand_sleep(t=None):
 	time.sleep(np.random.randint(3, 5) if not t else t)
 	print(get_readable_ctime())
@@ -24,13 +22,14 @@ def t2_func():
 	for i in range(1000):
 		print(i, ", t2")
 
-t1 = Process(target=t1_func)
-t1.start()
+if __name__ == '__main__':
+	print(os.getcwd())
+	print(get_readable_ctime(), mp.cpu_count())
+	pool = mp.Pool(mp.cpu_count())
+	pool.apply(rand_sleep, args=(3, ))
+	pool.apply(rand_sleep, args=(5, ))
 
-t2 = Process(target=t2_func)
-t2.start()
-
-t3 = threading.Thread(target=rand_sleep(2))
+# t3 = threading.Thread(target=rand_sleep(2))
 # t3.start()
 
 #t1.join()
